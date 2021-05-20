2c527f474a9c6fdd72ca0a880b9bdd54cfca3563
Jackson Graff <jacksongraff@hotmail.com>
2021-05-20 13:02:49 -0300

    generate certificate

--- serverless-app/src/functions/generateCertificate.ts
+++ serverless-app/src/functions/generateCertificate.ts
@@ -1,9 +1,31 @@
+import chromium from "chrome-aws-lambda"
+import path from "path";
+import fs from "fs";
+import handlebars from "handlebars";
+import dayjs from "dayjs";
+import { S3 } from "aws-sdk"
+
 import { document } from "../utils/dynamodbClient"
 
 interface ICreateCertificate {
     id: string;
     name: string;
     grade: string;
+}
+
+interface ITemplate {
+    id: string;
+    name: string;
+    grade: string;
+    date: string;
+    medal: string
+}
+
+const compile = async function(data: ITemplate) {
+    const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs");
+    const html = fs.readFileSync(filePath, "utf-8");
+
+    return handlebars.compile(html)(data)
 }
 
 export const handle = async (event) => {
@@ -18,10 +40,54 @@ export const handle = async (event) => {
         },
     }).promise();
 
+    const medalPath = path.join(process.cwd(), "src", "templates", "selo.png")
+
+    const data: ITemplate = {
+        date: dayjs().format("DD/MM/YYYY"),
+        medal: fs.readFileSync(medalPath, "base64"),
+        grade,
+        name,
+        id,
+    }
+
+    const content = await compile(data)
+
+    const browser = await chromium.puppeteer.launch({
+        headless: true,
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+    });
+
+    const page = await browser.newPage();
+
+    await page.setContent(content);
+
+    const pdf = await page.pdf({
+        format: "a4",
+        landscape: true,
+        path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+        printBackground: true,
+        preferCSSPageSize: true,
+    });
+
+    await browser.close();
+
+    const s3 = new S3();
+
+    await s3.putObject({
+        Bucket: "serverless-ignite",
+        Key: `${id}.pdf`,
+        ACL: "public-read",
+        Body: pdf,
+        ContentType: "application/pdf",
+    }).promise();
+
     return {
         statusCode: 201,
         body: JSON.stringify({
             message: "Certificate created!",
+            url: `https://serverless-ignite.s3-sa-east-1.amazonaws.com/${id}.pdf`
         }),
         headers: {
             "Content-type": "application/json",
